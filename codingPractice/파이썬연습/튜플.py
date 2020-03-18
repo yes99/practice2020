6859a913f5b0a9c50ec1f71ffeec3674fd60020e
yes99 <yes950324@naver.com>
2020-03-18 23:45:06 +0900

    파이썬 추가 또

--- codingPractice/파이썬연습/튜플.py
+++ codingPractice/파이썬연습/튜플.py
@@ -1,10 +1,9 @@
-smartos = ("Android OS", "iOS", "Windows 10 Mobile")
+smartos=("Android OS", "iOS", "Windows 10 Mobile")
 
 print("튜블 테스트")
 print(smartos)
 
-smartos[1] = "iOS 10 Coming this fall"
-print(smartos)
+
 
 market = 1, 2, 3
 Apple, Google, Microsoft = market
