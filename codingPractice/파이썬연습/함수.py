27ea5c2a08d82294a19ed7e6d5307dd5033eb7ed
yes99 <yes950324@naver.com>
2020-03-19 14:52:59 +0900

    함수고급 함수

---
+++ codingPractice/파이썬연습/함수.py
@@ -0,0 +1,15 @@
+def print_star(star):
+    for num in range(0, star, 1):
+        print('*', end='')
+
+    print('')
+
+print_star(5)
+print_star(10)
+
+def max(a,b):
+    if a>=b:
+        return a
+    return b
+
+print(max(50,100))
