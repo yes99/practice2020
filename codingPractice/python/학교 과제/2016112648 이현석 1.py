6cb84f46ed6c6fdd79589547018667bd720333ac
yes99 <yes950324@naver.com>
2020-03-30 23:43:47 +0900

    학교과제 및 C연습

---
+++ codingPractice/python/학교 과제/2016112648 이현석 1.py
@@ -0,0 +1,7 @@
+num1=int(input("첫 번째 숫자?"))
+num2=int(input("두 번째 숫자?"))
+num3=int(input("세 번째 숫자?"))
+
+result = num1 * (num2 + num3)
+
+print(num1,"* (", num2, "+", num3,")=",result)
---
+++ codingPractice/python/학교 과제/2016112648 이현석 2.py
@@ -0,0 +1,4 @@
+name = input("당신의 이름은?")
+year = int(input("당신이 태어난 년도는?"))
+age= 2020 - year
+print(name+"의 나이는", age, "살입니다")
