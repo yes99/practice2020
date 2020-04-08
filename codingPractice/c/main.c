a56cc92afa0e27b1cdc55ed156e02fce4b048bda
yes99 <yes950324@naver.com>
2020-04-08 23:07:47 +0900

    달팽이 배열 파이썬 -> C

--- codingPractice/c/main.c
+++ codingPractice/c/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#define LOOPTIME 8
+
+void sum(int a[], int b){
+    for (int i=0;i<b;i++){
+        a[i]++;
+    }
+}
 
 int main()
 {
-    printf("dsjafk;lawjgklweajg;klewg\n");
+    int a[5] = {1,2,3,4,5};
+    int b=5;
 
+    sum(a,b);
+
+    printf("%d %d %d %d %d", a[0],a[1],a[2],a[3],a[4]);
+    
     return 0;
 }
---
+++ codingPractice/c/snail.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+int number;
+float MAX_NUM;
+printf("size of SQUARE: ");
+scanf("%d", &number);
+
+if (MAX_NUM % 2 == 1)
+{
+     
+}
+
+printf("%f\n", MAX_NUM); 
+printf("repeat time %f", MAX_NUM);
+
+}
