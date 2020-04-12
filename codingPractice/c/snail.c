510034502e05e2254de963629416ca2227372ee1
yes99 <yes950324@naver.com>
2020-04-12 16:10:38 +0900

    snail 함수 변환 전

--- codingPractice/c/snail.c
+++ codingPractice/c/snail.c
@@ -4,15 +4,29 @@ int main()
 {
 int number;
 float MAX_NUM;
+int cycle;
 printf("size of SQUARE: ");
 scanf("%d", &number);
 
-if (MAX_NUM % 2 == 1)
+if(number%2==1) //홀수다
 {
-     
+    cycle = number /2 +1 ;
+}
+else
+{
+    cycle= number /2 ;
 }
 
-printf("%f\n", MAX_NUM); 
-printf("repeat time %f", MAX_NUM);
+printf("\n\n\n%d", cycle);
+
+
+
+/*
+float number2 = number;
+
+printf("\n\n%f", number2/2);
+*/
+
+
 
 }
