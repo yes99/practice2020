313df056283da3ceef03591bb6c90b421a885b58
yes99 <yes950324@naver.com>
2020-04-12 17:40:48 +0900

    행렬 만들기 까지

--- codingPractice/c/snail.c
+++ codingPractice/c/snail.c
@@ -5,21 +5,34 @@ int roundfunction(int i);
 
 int main()
 {
-int number;
-float MAX_NUM;
-int modnum;
-printf("size of SQUARE: ");
-scanf("%d", &number);
+    int number;
+    float MAX_NUM;
+    int modnum;
+    printf("size of SQUARE: ");
+    scanf("%d", &number);
 
-modnum = roundfunction(number);
+    modnum = roundfunction(number);
+//반올림 해서 cycle 횟수 정하는거 완료
 
+//int square[number][number];
 
+    int square[10000][10000];
+//주석 처럼 하고 싶은데 안돼서 그냥 질러 놓음    
+    
+    /*
+    for (i=0; i<modnum; i++)
+    {
+        for(j=0; j<modnum; j++)
+        {
+            
+        }
+    }
+    */
 
-/*
-float number2 = number;
-
-printf("\n\n%f", number2/2);
-*/
+   int count =0;
+   int current_number =0;
+   int x=0, y=0;
+   // 일단 변수 선언
 
 
 
