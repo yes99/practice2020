d1d95cb182265a4aa7dd081d19fedd7e7e470b87
yes99 <yes950324@naver.com>
2020-04-12 16:35:27 +0900

    snail.c 함수 적용

--- codingPractice/c/snail.c
+++ codingPractice/c/snail.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 
+int roundfunction(int i);
+
+
 int main()
 {
 int number;
 float MAX_NUM;
-int cycle;
+int modnum;
 printf("size of SQUARE: ");
 scanf("%d", &number);
 
-if(number%2==1) //홀수다
-{
-    cycle = number /2 +1 ;
-}
-else
-{
-    cycle= number /2 ;
-}
-
-printf("\n\n\n%d", cycle);
+modnum = roundfunction(number);
 
 
 
@@ -28,5 +22,23 @@ printf("\n\n%f", number2/2);
 */
 
 
+
+}
+
+
+
+int roundfunction(int i)
+{
+    int cycle;
+    if(i%2==1) //홀수다
+    {
+    cycle = i /2 +1 ;
+    }
+    else
+    {
+    cycle = i /2 ;
+    }
+
+    return cycle;
 
 }
